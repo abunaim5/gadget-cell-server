2c2303561c720ff4fb12fc03ae55f09da594dfe5
Abu Naim <abunaim5@users.noreply.github.com>
2025-07-09 21:03:40 +0600

    added cart route and add product to cart api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { connectDB } from "./config/db";
 import { Request, Response } from "express";
 import productRoutes from './routes/product.routes'
 import trendingRoutes from './routes/trending.routes'
+import cartRoutes from './routes/cart.routes'
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -14,6 +15,7 @@ app.use(cors());
 
 app.use('/products', productRoutes);
 app.use('/trending', trendingRoutes);
+app.use('/cart', cartRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('GadgetCell server is running.');
---
+++ src/routes/cart.routes.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { Router } from "express";
+import { db } from "../config/db";
+// import { ObjectId } from "mongodb";
+
+const router = Router();
+const cartCollection = db.collection('cart');
+
+type CartProductListType = {
+    productId: string;
+    name: string;
+    brand: string;
+    image: string;
+    description: string;
+    price: number;
+    oldPrice: number;
+    category: string;
+    ratings: number;
+    createdAt: string;
+}
+
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const cartProduct: CartProductListType = req.body;
+        const query = {
+            productId: cartProduct.productId
+        };
+        const existingProduct = await cartCollection.findOne(query);
+        if (existingProduct) {
+            res.status(400).json({ message: 'Product already exist' });
+            return;
+        };
+        const result = await cartCollection.insertOne(cartProduct);
+        res.status(200).json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+export default router;
